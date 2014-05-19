5f2ab5117a3b577046244f43a32e42673aa809ed
Clayton Hunt <clayton.hunt@digitalcomply.com>
2014-05-19 08:29:25 -0400

    Finished FizzBuzz using bridge pattern, gave example of how bridge can modify output

--- FizzBuzz/Bridge/BridgeFizzBuzz.cs
+++ FizzBuzz/Bridge/BridgeFizzBuzz.cs
@@ -2,9 +2,34 @@
 {
     public class BridgeFizzBuzz : IFizzBuzzCommand
     {
+        private readonly IOutputBridge _bridge;
+
+        private class NumberBridge : IOutputBridge
+        {
+            public string Output(int value)
+            {
+                return value.ToString();
+            }
+        }
+
+        public BridgeFizzBuzz()
+        {
+            _bridge = new NumberBridge();
+        }
+
+        public BridgeFizzBuzz(IOutputBridge bridge)
+        {
+            _bridge = bridge;
+        }
+
         public string Execute(int value)
         {
-            
+            return _bridge.Output(value);
         }
+    }
+
+    public interface IOutputBridge
+    {
+        string Output(int value);
     }
 }
