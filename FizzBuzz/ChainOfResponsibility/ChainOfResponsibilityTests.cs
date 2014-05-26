5db53f8206d5c98947f62ef12428ac06b6fb9fd1
Clayton Hunt <claytonhunt_104@hotmail.com>
2014-05-26 07:40:10 -0400

    added chain of responsibility test class

---
+++ FizzBuzz/ChainOfResponsibility/ChainOfResponsibilityTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTest.ChainOfResponsibility
+{
+    [TestClass]
+    public class ChainOfResponsibilityTests : CoreTests
+    {
+        protected override void Setup()
+        {
+            Fb = new ChainOfResponsibilityFizzBuzz();
+        }
+    }
+
+    public class ChainOfResponsibilityFizzBuzz : IFizzBuzzCommand
+    {
+        public ChainOfResponsibilityFizzBuzz()
+        {
+            
+        }
+
+        public string Execute(int value)
+        {
+            return null;
+        }
+    }
+}
